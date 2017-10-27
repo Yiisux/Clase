f93aa36114583a681d0fb0e49458e142ed2895a7
Jesús Arteaga Durán 1 <jaduran@aulas.triana>
2017-10-27 14:26:49 +0200

    viernes27

---
+++ AndroidMiguel/Calculadora/js.js
@@ -0,0 +1,19 @@
+let x0 = document.getElementById('x0').value
+let x1 = document.getElementById('x1').value
+let x2 = document.getElementById('x2').value
+let x3 = document.getElementById('x3').value
+let x4 = document.getElementById('x4').value
+let x5 = document.getElementById('x5').value
+let x6 = document.getElementById('x6').value
+let x7 = document.getElementById('x7').value
+let x8 = document.getElementById('x8').value
+let x9 = document.getElementById('x9').value
+let xsuma = document.getElementById('xsuma').value
+let xresta = document.getElementById('xresta').value
+let xdividir = document.getElementById('xdividir').value
+let xigual = document.getElementById('xigual').value
+let xmultiplicar = document.getElementById('xmultiplicar').value
+
+function escribir(n){
+	 document.getElementById('pantalla').inerHTML = n
+}
