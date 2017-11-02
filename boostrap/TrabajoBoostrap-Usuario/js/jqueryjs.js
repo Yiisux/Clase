eb93cf887e3435401cbf87e20c22ea744cc6ba4b
Yiisux <32164740+Yiisux@users.noreply.github.com>
2017-11-02 14:11:58 +0100

    Add files via upload

---
+++ boostrap/TrabajoBoostrap-Usuario/js/jqueryjs.js
@@ -0,0 +1,22 @@
+$(function(){
+  $(document).on("click",".btn-danger",function(){
+    let tituloCard = $(this).parent().find(".borrarTabla").text();
+    let selectorCard = $(this).parent().parent();
+    let id = selectorCard.attr("class");
+    alert("Eliminado correctamente");
+    selectorCard.remove();
+  });
+    $(document).on("click","#btn-guardar-nota",function(){
+      let titulo = $("#Titulo").val();
+      let modelo = $("#Modelo").val();
+      let descipcrion = $("#descripcion").val();
+      let marca = $("#Marca").val();
+      $("#tablaaverias").append( '<tr>  <td>'+titulo+'</td>          <td>'+marca+'</td>          <td>'+modelo+'</td>             <td>'+descipcrion+'</td>      <td class="td-actions">            <a href="#" class="btn btn-sm btn-primary">Editar</a>            <a href="#" class="btn btn-sm btn-danger">Borrar</a>          </td></tr>' );
+      $('#modalNuevaNota').modal('hide');
+    });
+    //limpiar modal
+    $('.modal').on('hidden.bs.modal', function(){
+    		$(this).find('form')[0].reset(); //para borrar todos los datos que tenga los input, textareas, select.
+    		$("label.error").remove();  //lo utilice para borrar la etiqueta de error del jquery validate
+    	});
+});
